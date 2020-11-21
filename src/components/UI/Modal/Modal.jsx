ae4e6d86c4dbf52592d784b99cde625927107297
evgeni83 <evgeni.daniluk@yandex.ru>
2020-11-21 13:02:34 +0300

    Backdrop component implemented

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
 const Modal = (props) => {
     return (
-        <div className={ classes.Modal }
-             style={ {
-                 transform: props.show ? 'translate(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1' : '0',
-             } }>
-            { props.children }
-        </div>
+        <>
+            <Backdrop show={ props.show } clicked={ props.modalClosed }/>
+            <div className={ classes.Modal }
+                 style={ {
+                     transform: props.show ? 'translate(0)' : 'translateY(-100vh)',
+                     opacity: props.show ? '1' : '0',
+                 } }>
+                { props.children }
+            </div>
+        </>
     );
 };
 
