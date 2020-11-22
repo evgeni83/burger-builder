e84e39b70fdd7a0b58d5910345d78e66ae667e39
evgeni83 <evgeni.daniluk@yandex.ru>
2020-11-22 14:57:56 +0300

    Price added to the order summary

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -14,6 +14,7 @@ const OrderSummary = (props) => {
             <h3>Your Order</h3>
             <p>Burger with:</p>
             <ul>{ ingredientSummary }</ul>
+            <p><strong>Total Price: { props.price.toFixed(2) }</strong></p>
             <p>Proceed to checkout?</p>
             <Button btnType="Danger" clicked={ props.purchaseCancelled }>CANCEL</Button>
             <Button btnType="Success" clicked={ props.purchaseContinued }>CONTINUE</Button>
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -94,9 +94,11 @@ class BurgerBuilder extends Component {
         return (
             <>
                 <Modal show={ this.state.purchasing } modalClosed={ this.purchaseCancelHandler }>
-                    <OrderSummary ingredients={ this.state.ingredients }
-                                  purchaseCancelled={ this.purchaseCancelHandler }
-                                  purchaseContinued={ this.purchaseContinueHandler }/>
+                    <OrderSummary
+                        price={ this.state.totalPrice }
+                        ingredients={ this.state.ingredients }
+                        purchaseCancelled={ this.purchaseCancelHandler }
+                        purchaseContinued={ this.purchaseContinueHandler }/>
                 </Modal>
                 <Burger ingredients={ this.state.ingredients }/>
                 <BuildControls
