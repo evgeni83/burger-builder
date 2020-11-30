038de8b342cae40118e3d6eba8bf96f6698a0b61
evgeni83 <evgeni.daniluk@yandex.ru>
2020-11-30 11:20:28 +0300

    The mobile menu opens when you click on a menu word

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -13,10 +13,14 @@ class Layout extends React.Component {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }))
+    }
+
     render() {
         return (
             <>
-                <Toolbar/>
+                <Toolbar drawerToggleClicked={ this.sideDrawerToggleHandler }/>
                 <SideDrawer open={ this.state.showSideDrawer } closed={ this.sideDrawerClosedHandler }/>
                 <main className={ classes.Content }>{ this.props.children }</main>
             </>
