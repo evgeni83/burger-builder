35e8584ae50e76fbdfe57bdb531cce497fedba50
evgeni83 <evgeni.daniluk@yandex.ru>
2020-12-07 08:58:49 +0300

    Sending a POST request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://danevgen-burgerbuilder-default-rtdb.firebaseio.com/',
+    baseURL: 'https://danevgen-burgerbuilder-default-rtdb.firebaseio.com',
 });
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,6 +3,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Modal from '../../components/UI/Modal/Modal';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 5,
@@ -79,7 +80,24 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        console.log('continue');
+        // console.log('continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'John',
+                address: {
+                    street: 'Bigstreet 1',
+                    zipCode: '12345',
+                    country: 'Russia'
+                },
+                email: 'qwe@rty.zx'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render() {
